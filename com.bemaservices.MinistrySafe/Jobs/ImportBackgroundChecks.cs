025c1982d03ed4c719b9683331f3e7bfe3189091
Taylor Cavaletto <tjcavaletto@gmail.com>
2025-05-22 16:00:01 -0400

    - Updated Obsolete Methods

--- com.bemaservices.MinistrySafe/Jobs/ImportBackgroundChecks.cs
+++ com.bemaservices.MinistrySafe/Jobs/ImportBackgroundChecks.cs
@@ -22,6 +22,7 @@ using System.Linq;
 using Quartz;
 
 using Rock;
+using Rock.Jobs;
 using Rock.Attribute;
 using Rock.Data;
 using Rock.Model;
@@ -39,7 +40,7 @@ namespace com.bemaservices.MinistrySafe.Jobs
     [SlidingDateRangeField( "Date Range", "The date range of trainings to import.", required: true )]
     [WorkflowTypeField( "Workflow Type", "An optional workflow type to fire for background checks without an existing workflow.", required: false )]
     [DisallowConcurrentExecution]
-    public class ImportBackgroundChecks : IJob
+    public class ImportBackgroundChecks : RockJob
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="ImportTrainings" /> class.
@@ -52,17 +53,16 @@ namespace com.bemaservices.MinistrySafe.Jobs
         /// Executes the specified context.
         /// </summary>
         /// <param name="context">The context.</param>
-        public virtual void Execute( IJobExecutionContext context )
+        public override void Execute( )
         {
-            JobDataMap dataMap = context.JobDetail.JobDataMap;
-            var dateRange = SlidingDateRangePicker.CalculateDateRangeFromDelimitedValues( dataMap.Get( "DateRange" ) != null ? dataMap.Get( "DateRange" ).ToString() : "-1||" );
+            var dateRange = SlidingDateRangePicker.CalculateDateRangeFromDelimitedValues( GetAttributeValue( "DateRange" ) != null ? GetAttributeValue( "DateRange" ).ToString() : "-1||" );
 
             int backgroundChecksProcessed = 0;
             List<string> errorMessages = new List<string>();
 
             var rockContext = new RockContext();
             WorkflowTypeCache workflowType = null;
-            Guid? workflowTypeGuid = dataMap.Get( "WorkflowType" ).ToStringSafe().AsGuidOrNull();
+            Guid? workflowTypeGuid = GetAttributeValue( "WorkflowType" ).ToStringSafe().AsGuidOrNull();
             if ( workflowTypeGuid.HasValue )
             {
                 var workflowTypeService = new WorkflowTypeService( rockContext );
@@ -72,7 +72,7 @@ namespace com.bemaservices.MinistrySafe.Jobs
             var ministrySafe = new MinistrySafe();
             ministrySafe.ImportBackgroundChecks( dateRange, workflowType, out backgroundChecksProcessed, out errorMessages );
 
-            context.Result += string.Format( "{0} background checks processed{1}", backgroundChecksProcessed, ( errorMessages.Count > 0 ) ? ", but " + errorMessages.Count + " errors were reported" : string.Empty );
+            this.Result += string.Format( "{0} background checks processed{1}", backgroundChecksProcessed, ( errorMessages.Count > 0 ) ? ", but " + errorMessages.Count + " errors were reported" : string.Empty );
         }
     }
 }
